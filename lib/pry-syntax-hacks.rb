e89346b17801c224ef476fb69eea34ae0df6ce94
Conrad Irwin <conrad.irwin@gmail.com>
2012-02-28 05:34:23 -0800

    HAX0R command eval's too

--- lib/pry-syntax-hacks.rb
+++ lib/pry-syntax-hacks.rb
@@ -3,14 +3,33 @@ class Pry
 
   def retrieve_line(eval_string, *args)
     old_retrieve_line(eval_string, *args)
+    Pry.hack_line(eval_string)
+  end
 
+  def self.hack_line(eval_string)
     eval_string.gsub!(/\.(@[a-z0-9_]+)/, '.instance_variable_get("\1")')
     eval_string.gsub!(/\.:([a-z0-9_]+[?!]?)/, '.method(:\1)')
     eval_string.gsub!(/\.!([a-z0-9_]+[?!]?)([ \(])(?=(.*))/) { ".send#{$2}:#{$1}#{$3 == ""?"":","}" }
-    eval_string.gsub!(/([\s\[{>(<=])((\.\.\/)+)([a-z0-9_@]+)/) { "#{$1}_pry_.binding_or_raise(#{-1 - $2.size / 3}).eval('#{$4}')" }
+    eval_string.gsub!(/(^|[\s\[{>(<=])((\.\.\/)+)([a-z0-9_@]+)/) { "#{$1}_pry_.binding_or_raise(#{-1 - $2.size / 3}).eval('#{$4}')" }
+    eval_string
   end
 
   def binding_or_raise(n)
     binding_stack[n] or raise "no parent binding"
+  end
+
+  class Command
+    def target
+      unless @target.respond_to?(:unhacked_eval)
+        class << @target
+          alias_method :unhacked_eval, :eval
+
+          def eval(str, *args)
+            unhacked_eval(Pry.hack_line(str.dup), *args)
+          end
+        end
+      end
+      @target
+    end
   end
 end
--- pry-syntax-hacks.gemspec
+++ pry-syntax-hacks.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name = "pry-syntax-hacks"
-  s.version = "0.0.5"
+  s.version = "0.0.6"
   s.platform = Gem::Platform::RUBY
   s.author = "Conrad Irwin"
   s.email = "conrad.irwin@gmail.com"
